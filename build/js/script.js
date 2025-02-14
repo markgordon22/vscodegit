733ceeee55765c0f09a2c3d03dbb906cf2afa1b1
markgordon22 <72653213+markgordon22@users.noreply.github.com>
2025-02-14 17:42:10 +0000

    Update script.js

--- build/js/script.js
+++ build/js/script.js
@@ -63,7 +63,7 @@ $(function () {
     .setPin('#friend')
     .addTo(controller)
 
-  // wedwedewdwedwedwedwedwedwedwedwedwedwedwedwedwedwedwedwedwedwedwdwdwed
+  // updatescript
 
   let parachuteTween = new TimelineMax()
 
